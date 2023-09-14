871168106f0c69cf333aa50237ce8aac21bda823
oprahdubie <oprahdubie@yahoo.com>
2023-09-14 16:00:53 +0100

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -83,7 +83,7 @@ const Post: React.FC<PostProps> = (props) => {
 	return (
 		<>
 			<Head>
-				<meta property="og:title" content='' />
+				<meta property="og:title" content={post.excerpt} />
 				<link rel="canonical" href={`https://${host}/${path}`} />
 				<meta property="og:description" content={removeTags(post.excerpt)} />
 				<meta property="og:url" content={`https://${host}/${path}`} />
@@ -97,10 +97,10 @@ const Post: React.FC<PostProps> = (props) => {
 					property="og:image:alt"
 					content={post.featuredImage.node.altText || post.title}
 				/>
-				<title></title>
+				<title>{post.excerpt}</title>
 			</Head>
 			<div className="post-container">
-				<h1></h1>
+				<h1>{post.title}</h1>
 				<img
 					src={post.featuredImage.node.sourceUrl}
 					alt={post.featuredImage.node.altText || post.title}
